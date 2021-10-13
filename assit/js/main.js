372e353557ffc19d409b75ae318636fa99d69fc2
rota99C <rotaboolean@gmail.com>
2021-10-13 15:23:27 +0200

    creato struttura html, collegato vari file e dato prprieta di selezione dei numeri. creato i numeri tramite "for"

---
+++ assit/js/main.js
@@ -0,0 +1,39 @@
+//Scrivi un programma che esegua un ciclo da 1 a 100
+for(let i=1; i<=100; i++){
+    console.log(i);
+    
+//selezione multipli di 3 e 5 
+if(i % 3==0 && i % 5==0){
+    console.log("fizbuzz");
+}
+//selezione multipli di 3 
+else if(i % 3==0){
+    console.log(fizz);
+}
+//selezione multipli di 5
+else if(i % 5==0){
+    console.log(buzz)
+}
+
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
